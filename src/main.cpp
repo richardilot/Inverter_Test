5d233123e6a8e838d4b045ad3dddf31057b72dc1
Richard Ilot <richieilot@gmail.com>
2023-08-21 13:17:26 +0200

    Changed with no moving average filter. Only hard coded validation check.

--- src/main.cpp
+++ src/main.cpp
@@ -21,22 +21,6 @@ volatile int state = 0;
 int newdata = 0;
 int j = 0;
 
-// moving average filter
-const int SAMPLES_NUM = 2;
-uint counter = 0;
-
-uint64_t total_IVT_Current = 0;
-uint64_t total_LoadPlus_Voltage = 0;
-uint64_t total_IVT_Temp = 0;
-uint64_t total_Batt_Voltage = 0;
-uint64_t total_IVT_Power = 0;
-uint64_t total_Cell_Volt_Min = 0;
-uint64_t total_Cell_Volt_Avg = 0;
-uint64_t total_Cell_Volt_Max = 0;
-uint64_t total_Cell_Temp_Min = 0;
-uint64_t total_Cell_Temp_Avg = 0;
-uint64_t total_Cell_Temp_Max = 0;
-
 //==========TRANSLATED DATA=======//
 // 0x405
 uint8_t BMS_State = 0;
@@ -45,27 +29,25 @@ uint8_t tempRelay = 0;
 uint8_t Relays[8] = {0}; // index 0 = discharge, 1 = charge plus, 2 = charge gnd, 3 = hv plus, 4 = precharge, 5 = hv ground
 
 // 0x402
-uint32_t IVT_Current[SAMPLES_NUM];
+uint32_t IVT_Current = 0;
 
 // 0x403
-uint32_t LoadPlus_Voltage[SAMPLES_NUM];
-uint32_t IVT_Temp[SAMPLES_NUM];
+uint32_t LoadPlus_Voltage = 0;
+uint32_t IVT_Temp = 0;
 
 // 0x404
-uint32_t Batt_Voltage[SAMPLES_NUM];
-uint32_t IVT_Power[SAMPLES_NUM];
+uint32_t Batt_Voltage = 0;
+uint32_t IVT_Power = 0;
 
 // 0x400
-uint32_t Cell_Volt_Min[SAMPLES_NUM];
-uint32_t Cell_Volt_Avg[SAMPLES_NUM];
-uint32_t Cell_Volt_Max[SAMPLES_NUM];
+uint32_t Cell_Volt_Min = 0;
+uint32_t Cell_Volt_Avg = 0;
+uint32_t Cell_Volt_Max = 0;
 
 // 0x401
-uint32_t Cell_Temp_Min[SAMPLES_NUM];
-uint32_t Cell_Temp_Avg[SAMPLES_NUM];
-uint32_t Cell_Temp_Max[SAMPLES_NUM];
-
-
+uint32_t Cell_Temp_Min = 0;
+uint32_t Cell_Temp_Avg = 0;
+uint32_t Cell_Temp_Max = 0;
 
 uint64_t packet201 = 0;
 
@@ -85,75 +67,68 @@ void TranslatedData()
 
   Serial.print("Battery Modules\n");
   Serial.print("\t\tCell Volt Min: ");
-  float MA_Cell_Volt_Min = (float)total_Cell_Volt_Min / SAMPLES_NUM;
-  if (MA_Cell_Volt_Min > 5000 && MA_Cell_Volt_Min < 500)
+  if (Cell_Volt_Min > 5000 && Cell_Volt_Min < 500)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Volt_Min / 1000.0);
+    Serial.print((float)Cell_Volt_Min / 1000.0);
     Serial.print(" V");
   }
 
-
   Serial.print("\tCell Volt Avg: ");
-  float MA_Cell_Volt_Avg = (float)total_Cell_Volt_Avg / SAMPLES_NUM;
-  if (MA_Cell_Volt_Avg > 4000 && MA_Cell_Volt_Avg < 1000)
+  if (Cell_Volt_Avg > 4000 && Cell_Volt_Avg < 1000)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Volt_Avg / 1000.0);
+    Serial.print((float)Cell_Volt_Avg / 1000.0);
     Serial.print(" V");
   }
 
   Serial.print("\tCell Volt Max: ");
-  float MA_Cell_Volt_Max = (float)total_Cell_Volt_Avg / SAMPLES_NUM;
-  if (MA_Cell_Volt_Max > 4000 && MA_Cell_Volt_Max < 1000)
+  if (Cell_Volt_Max > 4000 && Cell_Volt_Max < 1000)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Volt_Max / 1000.0);
+    Serial.print((float)Cell_Volt_Max / 1000.0);
     Serial.print(" V");
   }
 
   Serial.print("\t\tCell Temp Min: ");
-  float MA_Cell_Temp_Min = (float)total_Cell_Temp_Min / SAMPLES_NUM;
-  if (MA_Cell_Temp_Min > 600 && MA_Cell_Temp_Min < 10)
+  if (Cell_Temp_Min > 600 && Cell_Temp_Min < 10)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Temp_Min / 10.0);
+    Serial.print((float)Cell_Temp_Min / 10.0);
     Serial.print(" degC");
   }
 
   Serial.print("\tCell Temp Avg: ");
-  float MA_Cell_Temp_Avg = (float)total_Cell_Temp_Avg / SAMPLES_NUM;
-  if (MA_Cell_Temp_Avg > 600 && MA_Cell_Temp_Avg < 10)
+  if (Cell_Temp_Avg > 600 && Cell_Temp_Avg < 10)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Temp_Avg / 10.0);
+    Serial.print((float)Cell_Temp_Avg / 10.0);
     Serial.print(" degC");
   }
-  
+
   Serial.print("\tCell Temp Max: ");
-  float MA_Cell_Temp_Max = (float)total_Cell_Temp_Max / SAMPLES_NUM;
-  if (MA_Cell_Temp_Max > 600 && MA_Cell_Temp_Max < 10)
+  if (Cell_Temp_Max > 600 && Cell_Temp_Max < 10)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Cell_Temp_Max / 10.0);
+    Serial.print(Cell_Temp_Max / 10.0);
     Serial.print(" degC");
   }
 
@@ -201,51 +176,47 @@ void TranslatedData()
   // Print readable Load+ Voltage
   Serial.print("\tIVT Data\n");
   Serial.print("\t\tLoad+ Voltage: ");
-  float MA_LoadPlus_Voltage = (float)total_LoadPlus_Voltage / SAMPLES_NUM;
-  if (MA_LoadPlus_Voltage > 300000)
+  if (LoadPlus_Voltage > 300000)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_LoadPlus_Voltage / 1000.0);
+    Serial.print((float)LoadPlus_Voltage / 1000.0);
     Serial.print(" V");
   }
 
   // Print readable Batt+ Voltage
   Serial.print("\tInput Voltage: ");
-  float MA_Batt_Voltage = (float)total_Batt_Voltage / SAMPLES_NUM;
-  if (MA_Batt_Voltage > 300000)
+  if (Batt_Voltage > 300000)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_Batt_Voltage / 1000.0);
+    Serial.print((float)Batt_Voltage / 1000.0);
     Serial.print(" V");
   }
 
   Serial.print("\tIVT Current: ");
-  float MA_IVT_Current = (float)total_IVT_Current / SAMPLES_NUM;
-  if (MA_IVT_Current > 100000)
+  if (IVT_Current > 20000)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_IVT_Current / 1000.0);
+    Serial.print((float)IVT_Current / 1000.0);
     Serial.print(" A");
   }
 
   Serial.print("\tIVT Temperature: ");
-  float MA_IVT_Temp = (float)total_IVT_Temp / SAMPLES_NUM;
-  if (MA_IVT_Temp > 500 && MA_IVT_Temp < 10)
+  if (IVT_Temp > 500 && IVT_Temp < 10)
   {
     Serial.print("N/A");
   }
   else
   {
-    Serial.print(MA_IVT_Temp / 10.0);
+    Serial.print((float)IVT_Temp / 10.0);
     Serial.println(" degC");
   }
   // Print readable HV Relays
@@ -332,78 +303,41 @@ void canSniff0(const CAN_message_t &msg)
   // to print on the console
   if (can0Listen)
   {
-    if (counter == SAMPLES_NUM)
+    switch (msg.id)
     {
-      TranslatedData();
-      counter = 0;
+    case 0x405:
+    {
+      tempRelay = msg.buf[3];
+      printBinary(tempRelay);
+      IMD_State = msg.buf[5];
+      BMS_State = msg.buf[7];
     }
-    else
+    case 0x402:
     {
-      total_Batt_Voltage -= Batt_Voltage[counter];
-      total_LoadPlus_Voltage -= LoadPlus_Voltage[counter];
-      total_IVT_Current -= IVT_Current[counter];
-      total_IVT_Power -= IVT_Power[counter];
-      total_IVT_Temp -= IVT_Temp[counter];
-      total_Cell_Volt_Min -= Cell_Volt_Min[counter];
-      total_Cell_Volt_Avg -= Cell_Volt_Avg[counter];
-      total_Cell_Volt_Max -= Cell_Volt_Max[counter];
-      total_Cell_Temp_Min -= Cell_Temp_Min[counter];
-      total_Cell_Temp_Avg -= Cell_Temp_Avg[counter];
-      total_Cell_Temp_Max -= Cell_Temp_Max[counter];
-
-      switch (msg.id)
-      {
-      case 0x405:
-      {
-        tempRelay = msg.buf[3];
-        printBinary(tempRelay);
-        IMD_State = msg.buf[5];
-        BMS_State = msg.buf[7];
-      }
-      case 0x402:
-      {
-        IVT_Current[counter] = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
-        
-        total_IVT_Current += IVT_Current[counter];
-      }
-      case 0x403:
-      {
-        LoadPlus_Voltage[counter] = ((uint64_t)msg.buf[0] << 24) + ((uint64_t)msg.buf[1] << 16) + ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
-        IVT_Temp[counter] = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
-        
-        total_LoadPlus_Voltage += LoadPlus_Voltage[counter];
-        total_IVT_Temp += IVT_Temp[counter];
-      }
-      case 0x404:
-      {
-        Batt_Voltage[counter] = ((uint64_t)msg.buf[0] << 24) + ((uint64_t)msg.buf[1] << 16) + ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
-        IVT_Power[counter] = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
-        
-        total_Batt_Voltage += Batt_Voltage[counter];
-        total_IVT_Power += IVT_Power[counter];
-      }
-      case 0x400:
-      {
-        Cell_Volt_Min[counter] = ((uint64_t)msg.buf[0] << 8) + ((uint64_t)msg.buf[1]);
-        Cell_Volt_Avg[counter] = ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
-        Cell_Volt_Max[counter] = ((uint64_t)msg.buf[4] << 8) + ((uint64_t)msg.buf[5]);
-        
-        total_Cell_Volt_Min += Cell_Volt_Min[counter];
-        total_Cell_Volt_Avg += Cell_Volt_Avg[counter];
-        total_Cell_Volt_Max += Cell_Volt_Max[counter];
-      }
-      case 0x401:
-      {
-        Cell_Temp_Min[counter] = ((uint64_t)msg.buf[0] << 8) + ((uint64_t)msg.buf[1]);
-        Cell_Temp_Avg[counter] = ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
-        Cell_Temp_Max[counter] = ((uint64_t)msg.buf[4] << 8) + ((uint64_t)msg.buf[5]);
-
-        total_Cell_Temp_Min += Cell_Temp_Min[counter];
-        total_Cell_Temp_Avg += Cell_Temp_Avg[counter];
-        total_Cell_Temp_Max += Cell_Temp_Max[counter];
-      }
-      }
-      counter++;
+      IVT_Current = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
+    }
+    case 0x403:
+    {
+      LoadPlus_Voltage = ((uint64_t)msg.buf[0] << 24) + ((uint64_t)msg.buf[1] << 16) + ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
+      IVT_Temp = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
+    }
+    case 0x404:
+    {
+      Batt_Voltage = ((uint64_t)msg.buf[0] << 24) + ((uint64_t)msg.buf[1] << 16) + ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
+      IVT_Power = ((uint64_t)msg.buf[4] << 24) + ((uint64_t)msg.buf[5] << 16) + ((uint64_t)msg.buf[6] << 8) + ((uint64_t)msg.buf[7]);
+    }
+    case 0x400:
+    {
+      Cell_Volt_Min = ((uint64_t)msg.buf[0] << 8) + ((uint64_t)msg.buf[1]);
+      Cell_Volt_Avg = ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
+      Cell_Volt_Max = ((uint64_t)msg.buf[4] << 8) + ((uint64_t)msg.buf[5]);
+    }
+    case 0x401:
+    {
+      Cell_Temp_Min = ((uint64_t)msg.buf[0] << 8) + ((uint64_t)msg.buf[1]);
+      Cell_Temp_Avg = ((uint64_t)msg.buf[2] << 8) + ((uint64_t)msg.buf[3]);
+      Cell_Temp_Max = ((uint64_t)msg.buf[4] << 8) + ((uint64_t)msg.buf[5]);
+    }
     }
   }
 }
